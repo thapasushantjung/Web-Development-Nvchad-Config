52b0f945c5064dd756fdb55052a31ee3da65cbf7
sushant <sushant46bit23@kcc.edu.np>
2026-06-15 00:13:18 +0545

    italics stylings support for treesitter added

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,12 +6,40 @@
 local M = {}
 
 M.base46 = {
-	theme = "material-deep-ocean",
+	theme = "tokyonight",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		-- 1. Variables and Parameters
+		["@variable.parameter"] = { italic = true },
+		["@parameter"] = { italic = true },
+		["@variable.builtin"] = { italic = true },
+		["@variable.implicit"] = { italic = true },
+
+		-- 2. Modern Structural Elements
+		["@keyword.storage"] = { italic = true },
+		["@storageclass"] = { italic = true },
+		StorageClass = { italic = true },
+
+		["@keyword.repeat"] = { italic = true },
+		["@repeat"] = { italic = true },
+		Repeat = { italic = true },
+
+		["@keyword.conditional"] = { italic = true },
+		["@conditional"] = { italic = true },
+		Conditional = { italic = true },
+
+		["@keyword.exception"] = { italic = true },
+		["@exception"] = { italic = true },
+		Exception = { italic = true },
+        
+        -- Language agnostic fallbacks (forces let, const, if, else)
+        Keyword = { italic = true },
+        Statement = { italic = true },
+
+		-- 3. Metadata and Markup
+		["@attribute"] = { italic = true },
+		["@tag.attribute"] = { italic = true },
+	},
 }
 
 -- M.nvdash = { load_on_startup = true }
